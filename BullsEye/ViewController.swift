43eaa9a1ac8edc611fa185beefaec8837959f35b
EasonX <yixinx@andrew.cmu.edu>
2017-01-29 22:17:44 -0800

    Add random generator and target value

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,13 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
+    var targetValue: Int = 0
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        super.viewDidLoad()
-        currentValue = lroundf(slider.value)
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,16 +29,29 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         let message = "The value of the slider is:\(currentValue)"
+                    + "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Hello, World",
                                       message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(slider: UISlider){
         currentValue = lroundf(slider.value)
         print(currentValue)
+    }
+    
+    func startNewRound(){
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
     }
 }
 
