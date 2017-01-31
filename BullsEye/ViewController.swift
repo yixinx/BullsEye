4a7089be12163b6b4a658779e28f725c2eb31dac
EasonX <yixinx@andrew.cmu.edu>
2017-01-30 22:01:14 -0800

    Make everything look good!

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -22,6 +22,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImagehighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImagehighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftTmage = UIImage(named: "SliderTrackLeft"){
+            let trackLeftResizable = trackLeftTmage.resizableImage(withCapInsets: insets)
+            slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        }
+        if let trackRightImage = UIImage(named: "SliderTrackRight"){
+            let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+            slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        }
         startNewGame()
         updateLabels()
     }
